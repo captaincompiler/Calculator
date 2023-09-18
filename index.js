90c2e5a2bf9dad9afae253e71c3a792ad4c1ff05
captaincompiler <143388690+captaincompiler@users.noreply.github.com>
2023-09-18 08:37:11 -0700

    calculator

---
+++ index.js
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+let data = await inquirer.prompt([
+    {
+        type: "list",
+        name: "operations",
+        message: "chose your operation",
+        choices: ["addition", "subtraction", "multiplication", "division", "modolus", "exponent"]
+    }, {
+        type: "number",
+        name: "num1",
+        message: "enter your first number"
+    }, {
+        type: "number",
+        name: "num2",
+        message: "enter your second number"
+    }
+]).then((answer) => {
+    console.log(answer);
+    switch (answer.operations) {
+        case "addition":
+            console.log(answer.num1 + answer.num2);
+            break;
+        case "subtraction":
+            console.log(answer.num1 - answer.num2);
+            break;
+        case "multiplication":
+            console.log(answer.num1 * answer.num2);
+            break;
+        case "division":
+            console.log(answer.num1 / answer.num2);
+            break;
+        case "modolus":
+            console.log(answer.num1 % answer.num2);
+            break;
+        case "exponent":
+            console.log(answer.num1 ** answer.num2);
+        default:
+            break;
+    }
+});
