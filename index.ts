330cba52ef74311069d2ad8c6e4d3288eaeb79da
captaincompiler <143388690+captaincompiler@users.noreply.github.com>
2023-09-19 08:09:21 -0700

    index

--- index.ts
+++ index.ts
@@ -1,49 +1,49 @@
 import inquirer from "inquirer";
 let data = await inquirer.prompt([
     {
-        type:"list",
-        name:"operations",
-        message:"chose your operation",
-        choices:["addition","subtraction","multiplication","division","modolus","exponent"]
-    },{
+        type: "list",
+        name: "operations",
+        message: "chose your operation",
+        choices: ["addition", "subtraction", "multiplication", "division", "modolus", "exponent"]
+    }, {
         type: "number",
-        name:"num1",
-        message:"enter your first number"
+        name: "num1",
+        message: "enter your first number"
 
-    },{
-        type:"number",
-        name:"num2",
-        message:"enter your second number"
+    }, {
+        type: "number",
+        name: "num2",
+        message: "enter your second number"
     }
-]).then((answer)=>{
+]).then((answer) => {
     console.log(answer);
     switch (answer.operations) {
         case "addition":
-            console.log(answer.num1+answer.num2);
+            console.log(answer.num1 + answer.num2);
 
             break;
         case "subtraction":
-            console.log(answer.num1-answer.num2);
-              
+            console.log(answer.num1 - answer.num2);
+
             break;
-            case "multiplication":
-                console.log(answer.num1*answer.num2);
+        case "multiplication":
+            console.log(answer.num1 * answer.num2);
 
-                break;
-                case "division":
-                    console.log(answer.num1/answer.num2);
+            break;
+        case "division":
+            console.log(answer.num1 / answer.num2);
 
-                    break;
-                    case "modolus":
-                        console.log(answer.num1%answer.num2);
+            break;
+        case "modolus":
+            console.log(answer.num1 % answer.num2);
 
-                        break;
-                        case "exponent":
-                            console.log(answer.num1**answer.num2);
-                            
-                        
+            break;
+        case "exponent":
+            console.log(answer.num1 ** answer.num2);
+
+
         default:
             break;
     }
-    
+
 })
